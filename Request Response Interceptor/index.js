e5297972c029eedf5c7bb8aea66c5d6fba06f34b
Aditya Rao <adityarao@Farzis-MacBook-Pro.local>
2023-02-12 20:22:02 +0530

    index.html and info added

--- Request Response Interceptor/index.js
+++ Request Response Interceptor/index.js
@@ -3,35 +3,30 @@ const originalFetch = window.fetch;
 window.requestInterceptor = (args) => {
     // perform some action
     console.log("inside requestInterceptor", args);
-    
+
     return args;
 }
 
 
 window.responseInterceptor = (response) => {
     // perform some actions
-     console.log("inside responseInterceptor", response);
-     
-     
-     return response.JSON();
+    console.log("inside responseInterceptor", response);
+
+
+    return response.json();
 }
-window.fetch = async(...args)=> {
+window.fetch = async(...args) => {
     const updatedRequest = requestInterceptor(args);
-    
-    
+
+
     const response = await originalFetch(...updatedRequest);
-    
-    
-    const updatedResponse =  responseInterceptor(response);
-    
+
+
+    const updatedResponse = responseInterceptor(response);
+
     return updatedResponse;
 };
 
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
- .then(response => console.log(response))
-
-
-
-
-
+    .then(response => console.log(response))
